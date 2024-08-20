aed9dedbfa603eb8a4ba63e3fb109d925572d939
ikinoti <gitongakinoti1@gmail.com>
2024-08-20 20:39:22 +0300

    function to list all items in the todo list

--- todo.py
+++ todo.py
@@ -5,8 +5,8 @@ import datetime
 # help function
 def help():
     sa = """
-        $ ./todo add "todo item" # Add a new todo
-        $ ./todo ls               # Show remaining todos
+        $ ./todo add_items "todo item" # Add a new todo
+        $ ./todo list_items               # Show remaining todos
         $ ./todo del NUMBER       # Delete a todo
         $ ./todo done NUMBER      # Complete a todo
         $ ./todo help             # Show usage
@@ -23,3 +23,17 @@ def add_items(s):
     file.close()
     s = '"'+s+'"'
     print(f'Added todo: {s}')
+
+# Function to print items in the todo list
+def list_items():
+    try:
+        nec()
+        l = len(d)
+        k = l
+
+        for i in d:
+            sys.stdout.buffer.write(f'[{l}] {d[l]}'.encode('utf8'))
+            sys.stdout.buffer.write("\n".encode('utf8'))
+            l = l - 1
+    except Exception as e:
+        raise e
